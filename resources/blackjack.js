58edfe27091efb7b7b524c0a1dcf54c5f52efcf5
Anthony Deng <andeng2@berkeley.edu>
2023-01-04 14:49:02 -0800

    game logic finished

--- resources/blackjack.js
+++ resources/blackjack.js
@@ -1,13 +1,27 @@
-const deck = []
-const playerHand = []
-const houseHand = []
+const deck = buildDeck();
+const playerHand = [];
+const houseHand = [];
 
-window.onload = () => 
-    initGame();
+window.onload = function() {
 
-function initGame() {
+    newGame();
+    renderHands(true);
+    
+    document.getElementById("draw").addEventListener("click", draw);
+    document.getElementById("play").addEventListener("click", play);
 
-    buildDeck();
+}
+
+function newGame() {
+
+    deck.push(...playerHand);
+    deck.push(...houseHand);
+
+    playerHand.length = 0;
+    houseHand.length = 0;
+
+    document.getElementById("result").innerHTML = "";
+    
     shuffleDeck();
 
     addCard(playerHand);
@@ -16,9 +30,12 @@ function initGame() {
     addCard(houseHand);
     addCard(houseHand);
 
+
 }
 
 function buildDeck() {
+
+    let deck = [];
 
     let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let types = ['D', 'C', 'H', 'S'];
@@ -28,6 +45,8 @@ function buildDeck() {
             deck.push(values[i] + '_' + types[j]);
         }
     }
+
+    return deck;
 
 }
 
@@ -100,12 +119,153 @@ function evalHand(hand, isMax, isBest) {
 
 }
 
-function isBust(player) {
+function isBust() {
 
-    return evalHand(player, false, false) <= 21;
+    return evalHand(playerHand, false, false) > 21;
 
 }
 
+function renderHands(isOver) {
+
+    // House hand
+    let hand = document.getElementsByClassName("house-cards")[0];
+    hand.innerHTML = "";
+
+    var cardImg;
+
+    // If the game is not over, hide the first card.
+    if (isOver) {
+
+        cardImg = document.createElement("img");
+        cardImg.src = "cards/back.png";
+        hand.append(cardImg); 
+
+        for (let i = 1; i < houseHand.length; i++) {
+            cardImg = document.createElement("img");
+            cardImg.src = "cards/" + houseHand[i] + ".png";
+            hand.append(cardImg);   
+        }
+        
+    }
+
+    // Reveal the dealer's card since the game is over.
+    else {
+
+        for (let i = 0; i < houseHand.length; i++) {
+            cardImg = document.createElement("img");
+            cardImg.src = "cards/" + houseHand[i] + ".png";
+            hand.append(cardImg);   
+        }
+
+    }
+
+    // The player's hand
+    hand = document.getElementsByClassName("self-cards")[0];
+    hand.innerHTML = ""; 
+    
+    for (let i = 0; i < playerHand.length; i++) {
+        cardImg = document.createElement("img");
+        cardImg.src = "cards/" + playerHand[i] + ".png";
+        hand.append(cardImg);
+    }
+
+}
+
+function draw() {
+
+    addCard(playerHand);
+    renderHands(true);
+
+    if (isBust()) {
+        endGame();
+    }
+
+}
+
+function play() {
+    endGame();
+}
+
+function endGame() {
+
+    let houseScore = evalHand(houseHand, true, true);
+    while (houseScore < 17) {
+        addCard(houseHand);
+        houseScore = evalHand(houseHand, true, true);
+    }
+    
+    renderHands(false);
+
+    var msg;
+    let playerScore = evalHand(playerHand, true, true);
+
+    if (playerScore > 21) {
+        msg = 'You LOSE!';
+    }
+
+    else if (houseScore > 21) {
+        msg = 'You WIN!';
+    }
+
+    else if (playerScore == houseScore) {
+
+        if (playerScore == 21) {
+
+            if (playerHand.length == 2) {
+                
+                if (houseHand.length == 2) {
+
+                    msg = 'TIE!';
+
+                }
+
+                else {
+
+                    msg = 'You WIN!';
+
+                }
+
+            }
+
+            else {
+
+                if (houseHand.length == 2) {
+
+                    msg = 'You LOSE!';
+
+                }
+
+                else {
+
+                    msg = 'TIE!';
+
+                }
+
+            }
+
+        }
+
+        else {
+            msg = 'TIE!';
+        }
+        
+    }
+
+    else if (playerScore > houseScore) {
+
+        msg = 'You WIN!';
+
+    }
+
+    else {
+
+        msg = 'YOU LOSE!';
+
+    }
+
+    document.getElementById("result").innerHTML = msg;
+
+}
 
 
 
